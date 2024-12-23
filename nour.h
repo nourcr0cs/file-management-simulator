2a73a1316630de711d3b7b9167231debda61fdce
nourcr0cs <nourimene2005@gmail.com>
2024-12-23 16:08:22 +0100

    headerfile

---
+++ nour.h
@@ -0,0 +1,38 @@
+#ifndef L_OF_H
+#define L_OF_H
+
+void initMS(MS *ms, int nbrbloc);
+
+void initMetadonnees(FILE *disque, int i);
+
+void initTableAllocation(MS* ms);
+
+void creationL_OF(FILE *disque, MS *ms, int nbrbloc);
+
+adressemetadonnees recherchemetadonnees(FILE*disque,const char* nomfichier);
+
+int liremetadonnees(FILE*disque, const char* nomFichier, int caracteristique);
+
+bool ajoutermetadonnes(FILE* disque, fichiermetadonnees metadonnes, int taille);
+
+void miseAJourMetadonnees(FILE* disque, const char* nomFichier, int champ, int nouvelleValeur);
+
+void defregmentation(FILE *disque, MS *ms, const char *nomFichier);
+
+void MAJtaballocation(MS *ms, int index, int etat);
+
+void MAJMetadonnees(FILE *disque, const char *nomFichier, int champ, int nouvelleValeur);
+
+maladie insertHelper();
+
+void insertDis(FILE *disque, MS *ms, int nbrbloc, const char* nomFichier);
+
+bool researchDis(FILE *disque, MS *ms, int searchId, const char* nomFichier, int* pt, int* indx);
+
+void suppLogique(FILE *disque, MS *ms, int searchId, const char *nomFichier);
+
+void suppPhysique(FILE *disque, MS *ms, const char *nomFichier);
+
+void renameFile(FILE *disque, const char *nomFichier, const char *newName);
+
+#endif 
